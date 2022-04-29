612c694e65a394e7edcc26ba7a6170ca3ab61c59
Rodrigo Galhardo <6187392+rodrigogalhardo@users.noreply.github.com>
2022-04-29 20:29:22 -0300

    Create web-crawling.py

---
+++ web-crawling.py
@@ -0,0 +1,35 @@
+"""
+WEB CRAWLER P/ FINS DIDATICOS
+
+BY R. Galhardo | CISO, Ethical Hacking 
+"""
+
+import requests
+import re
+
+to_crawl = ['http://seu-dominio.com.br']
+crawled = set()
+
+header = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                        'AppleWebKit/537.36 (KHTML, like Gecko) '
+                        'Chrome/51.0.2704.103 Safari/537.36'}
+
+while True:
+    url = to_crawl[0]
+    try:
+        req = requests.get(url, headers=header)
+    except:
+        to_crawl.remove(url)
+        crawled.add(url)
+        continue
+
+    html = req.text
+    links = re.findall(r'<a href="?\'?(https?:\/\/[^"\'>]*)', html)
+    print('Crawling:', url)
+
+    to_crawl.remove(url)
+    crawled.add(url)
+
+    for link in links:
+        if link not in crawled and link not in to_crawl:
+            to_crawl.append(link)
